7d449f55f8ab07b2a8af13b509cb85a6859f62b7
kjaffel <khawla.jaffel@cern.ch>
2021-03-30 20:03:36 +0200

    add hit resolution measurment, WIP resolution as function of cluster width

--- test/CPE4slurm.py
+++ test/CPE4slurm.py
@@ -10,9 +10,9 @@ from CP3SlurmUtils.Configuration import Configuration
 from CP3SlurmUtils.SubmitWorker import SubmitWorker
 
 import logging
-logger = logging.getLogger("CPEAnalyser->SkimProducer")
+logger = logging.getLogger("CPEAnalyser: algorithm used to determine the position and errors of the CMS Strip clusters")
 
-def RunSKIMMER(yml=None, outputDIR= None, isTest=False):
+def ClusterParameterEstimator_4SLURM(yml=None, outputDIR= None, task=None, isTest=False):
     config = Configuration()
     config.sbatch_partition = 'cp3'
     config.sbatch_qos = 'cp3'
@@ -21,11 +21,11 @@ def RunSKIMMER(yml=None, outputDIR= None, isTest=False):
     config.sbatch_time = '0-02:00'
     sbatch_memPerCPU = '2000'
     #config.environmentType = 'cms'
-    config.inputSandboxContent = ["skimProducer_cfg.py"]
+    config.inputSandboxContent = ["skimProducer.py" if task=="skim" else("SiStripHitResol.py" if task=="HitResol" else("CPEstimator.py"))]
     config.stageoutFiles = ['*.root']
     config.stageoutDir = config.sbatch_chdir
     config.inputParamsNames = ["inputfile","outputfile"]
-    #yaml_path = os.path.join(config.cmsswDir, '/SiStripCPE/CPEanalyzer/test/alcareco_2018analysis.yml')
+    
     yaml_path = os.path.join(config.cmsswDir, options.yml)
     with open(yaml_path,"r") as file:
         ymlConfiguration = yaml.load(file)#,Loader=yaml.FullLoader)
@@ -49,11 +49,19 @@ def RunSKIMMER(yml=None, outputDIR= None, isTest=False):
             if not os.path.exists(outputdir_Persmp):
                 os.makedirs(outputdir_Persmp)
             config.inputParams = [ [ ",".join(input), os.path.join(outputdir_Persmp, "Output_%s.root"%idx) ] for idx,input in enumerate(sliced) ]
+    
     config.payload = \
     """
     echo ${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}
-    cmsRun skimProducer_cfg.py inputFiles=${inputfile} outputFile=${outputfile}
+    if [ "$task" = "skim" ]; then
+        cmsRun skimProducer.py inputFiles=${inputfile} outputFile=${outputfile}
+    elif [ "$task" = "HitResol" ]; then
+        cmsRun SiStripHitResol.py inputFiles=${inputfile} outputFile=${outputfile}
+    else
+        cmsRun CPEstimator.py inputFiles=${inputfile} outputFile=${outputfile}
+    fi
     """
+    
     submitWorker = SubmitWorker(config, submit=True, yes=True, debug=True, quiet=True)
     submitWorker()
     
@@ -63,9 +71,16 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--outputdir', action='store', dest='outputdir', type=str, default=os.makedirs("." + current_time), help='** HistFactory output path')
     parser.add_argument('-y', '--yml', action='store', dest='yml', type=str, default=None, help='** Yml file that include your AlcaReco samples')
     parser.add_argument('--isTest', action='store_true', help='')
+    parser.add_argument('--task', action='store', choices= ["skim", "HitResol"], help='')
     options = parser.parse_args()
+    
     if os.path.exists(options.outputdir):
         logger.warning("Output directory {} exists, previous results may be overwritten".format( options.outputdir))
+    if isTest:
+        YmlFile ='../configs/alcareco_2018D_localtest.yml'
+    else:
+        YmlFile = options.yml
     print ('slurm Output dir :', options.outputdir)
-    print ('yaml input files :', options.yml)
-    RunSKIMMER(yml=options.yml, outputDIR= options.outputdir, isTest = options.isTest) 
+    print ('yaml input files :', YmlFile)
+    
+    ClusterParameterEstimator_4SLURM(yml=YmlFile, outputDIR= options.outputdir, task=options.task, isTest = options.isTest) 
