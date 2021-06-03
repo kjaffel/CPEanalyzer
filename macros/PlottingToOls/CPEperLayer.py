a527c67564d4a8bd8484160d4956fb0a207063de
kjaffel <khawla.jaffel@cern.ch>
2021-06-03 12:59:02 +0200

    more update to the cpe plotter script

--- macros/PlottingToOls/CPEperLayer.py
+++ macros/PlottingToOls/CPEperLayer.py
@@ -20,7 +20,7 @@ def Cluster_parameter_estimator_values(path=None):
         ymlConfiguration = yaml.load(file, Loader=yaml.FullLoader)
     
     for smp, cfg in ymlConfiguration["samples"].items():
-        for unit in ["PitchUnits", "Centimetres"]:
+        for unit in ["Centimetres"]: # PitchUnits
             hitreso_values = os.path.join(os.path.dirname(os.path.abspath(__file__)), path, "outputs", smp, "HitResolutionValues", "HitResolutionValues_{}_ALCARECO.txt".format(unit))
             if not os.path.exists(hitreso_values):
                 continue
@@ -36,17 +36,20 @@ def Cluster_parameter_estimator_values(path=None):
                         if 'Layer' in line:
                             continue
                         res  = float(vals[1])*10000
+                        err = float(vals[5])*10000
                         if "TIB_L%s"%idx in vals[0]: 
                             plt.scatter( 1., res, color=colors[idx], marker=markers[idx-1], label='cluster width = %s'%idx)
+                            plt.errorbar(1., res, yerr=err)
                         elif "TOB_L%s"%idx in vals[0]:
                             plt.scatter( 2., res, color=colors[idx], marker=markers[idx-1])
-            ax.set_xticks(np.arange(2))
-            ax.set_xticklabels(["TIB 1-3", "TOB 1-3 "], rotation=20., fontsize=8)
+                            plt.errorbar(2., res, yerr=err)
+            ax.set_xticks([1., 2.])
+            ax.set_xticklabels(["TIB 1-3", "TOB 1-3"], rotation=20., fontsize=8)
             plt.ylabel(r'SiStrip Hit Resolution. $(\mu m)$', fontsize=12.)
-            plt.legend()
+            plt.legend(loc="upper left")
             plt.title('CMS Preliminary', fontsize=12., loc='left')
             plt.title(r'$31.93 fb^{-1}$ (%s, 13TeV)'%(smp.split('_')[-1]), fontsize=12., loc='right')
-            plt.ylim(0., 70.)
+            plt.ylim(0., 80.)
             fig.savefig('{}/hit_resolution_{}_{}.png'.format(plots_dir, smp, unit))
             plt.gcf().clear()
     print(" plots can be found in : {}".format(plots_dir))
